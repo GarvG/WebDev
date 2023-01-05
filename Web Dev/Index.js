674209abfabd5f2f97a23c4e6699bfc130099faf
GarvG <67434601+GarvG@users.noreply.github.com>
2023-01-05 23:06:33 +0530

    js Hoisting concept

--- Web Dev/Index.js
+++ Web Dev/Index.js
@@ -1,3 +1,22 @@
-console.log("hi");
-document.getElementById('first').innerHTML="HEY";
+// console.log("hi");
+// document.getElementById('first').innerHTML="HEY";
 // loop
+console.log(name);
+var name;
+console.log(name);
+name="Agrv";
+console.log(name);
+fn();
+function fn(){
+    console.log("hello");
+}
+fn();
+function fn(){
+    console.log("hello");
+}
+//fnContainer();
+var fnContainer=function()
+{
+    console.log("I am Expression");
+}
+fnContainer();
