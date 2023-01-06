5c38777e9758568896a1437b488d9086290ee804
GarvG <67434601+GarvG@users.noreply.github.com>
2023-01-06 23:49:23 +0530

    lexical scoping,tdz

--- Web Dev/Index.js
+++ Web Dev/Index.js
@@ -1,22 +1,25 @@
 // console.log("hi");
 // document.getElementById('first').innerHTML="HEY";
 // loop
-console.log(name);
-var name;
-console.log(name);
-name="Agrv";
-console.log(name);
-fn();
-function fn(){
-    console.log("hello");
-}
-fn();
-function fn(){
-    console.log("hello");
-}
-//fnContainer();
-var fnContainer=function()
-{
-    console.log("I am Expression");
-}
-fnContainer();
+// console.log(name);
+// var name;
+// console.log(name);
+// name="Agrv";
+// console.log(name);
+// fn();
+// function fn(){
+//     console.log("hello");
+// }
+// fn();
+// function fn(){
+//     console.log("hello");
+// }
+// //fnContainer();
+// var fnContainer=function()
+// {
+//     console.log("I am Expression");
+// }
+// fnContainer();
+var a=10; 
+let a=10;
+const a=10;
