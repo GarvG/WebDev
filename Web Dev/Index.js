ac446662c2a368a901be4cd7e993de5ae70315d8
GarvG <67434601+GarvG@users.noreply.github.com>
2023-01-07 23:19:04 +0530

    every array is object in js

--- Web Dev/Index.js
+++ Web Dev/Index.js
@@ -20,6 +20,31 @@
 //     console.log("I am Expression");
 // }
 // fnContainer();
-var a=10; 
-let a=10;
-const a=10;
+// var a=10; 
+// let a=10;
+// const a=10;
+
+// let object={
+//     name:"Garv",
+//     lname:"Garg",
+//     sayHi:function()
+// {
+//     console.log("says hi");
+// }
+// }
+// console.log(object);
+
+// for(let key in object){
+//     console.log(key," : ",object[key]);
+// }
+
+// arrays
+
+let arr=[1,2,3,4];
+arr.myprop="hello";
+arr.Print=function(){
+    console.log("HEY")
+}
+for(let key in arr){
+    console.log(key," : ",arr[key]);
+}
