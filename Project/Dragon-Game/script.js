434c776afb413f622f43d535060d2ee1f70f13d3
GarvG <67434601+GarvG@users.noreply.github.com>
2023-01-14 13:07:35 +0530

    finished commenting

--- Project/Dragon-Game/script.js
+++ Project/Dragon-Game/script.js
@@ -3,8 +3,8 @@ cross=true;
 let audio = new Audio('music.mp3');
 let audiogo = new Audio('gameover.mp3');
 
-setTimeout(() => {
-    audio.play();
+setTimeout(() => {                                                                // At the Starting we are playing the introSong 
+    audio.play();                                                                 // after 1 second
 }, 1000)
 document.onkeydown = function (e) {                                                // keydown means any key press ie key press down 
     console.log("key code is ", e.keyCode)
@@ -20,9 +20,9 @@ document.onkeydown = function (e) {                                             
 
     if (e.keyCode == 39) {                                                                // for leftkey code is 39 
         dino = document.querySelector('.dino');                                           // for selecting the dino class  
-        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'))   // we are using dino classs and getting the current left value of dino at moment  
-        dino.style.left = (dinoX + 112 )+ "px";                                         // and moving it left we are adding 112 px to it ie as we press left key 
-                                                                                       // to the current position of dino 112 px will get added                
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'))    // we are using dino classs and getting the current left value of dino at moment  
+        dino.style.left = (dinoX + 112 )+ "px";                                           // and moving it left we are adding 112 px to it ie as we press left key 
+                                                                                           // to the current position of dino 112 px will get added                
     }
 
 
@@ -69,24 +69,35 @@ setInterval(() => {
         },2000)
     }
     else if
-        (offsetX <145 && cross){                                  // here is the condition if we do not for the collision 
-            score+=1;
-            updateScore(score);
-            cross=false;
-            setTimeout(()=>{
-                cross=true;
+        (offsetX <145 && cross){                                     // here is the condition if we do not collide with the obstacle
+            score+=1;                                                // so we are updating the score by +1 
+            updateScore(score);                                      // we are using a function updateScore where we are passing the current 
+                                                                     // the objective of the function is to display the updated score in the screen
+            
+            
+            cross=false;                                            // Since we cross the obstacle once we are making it false again 
+            setTimeout(()=>{                                        // again on the hope that 2nd time also we will cross the obstacle 
+                                                                    // so after 1 second we are making the cross true;               
+                cross=true;                                         // this cross false and true will keep on happening till we donot collide  
             },1000);
+
             setTimeout(()=>{
-                aniDur=parseFloat(window.getComputedStyle(obstacle,null).getPropertyValue('animation-duration'));
-                newDur=aniDur - 0.1;
-                obstacle.style.animationduration=newDur + 's';
+                aniDur=parseFloat(window.getComputedStyle(obstacle,null).getPropertyValue('animation-duration'));    // Since we are the score is getting increase 
+                                                                                                                      // we want that our obstacle speed get increase
+                                                                                                                    // aniDur gives us the animation speed
+                                                                                                                    
+                newDur=aniDur - 0.1;                                                                                 // for making the obstacle speed up we are subtracting
+                                                                                                                    // 0.1s from the current animation and storing it in newDur
+
+                obstacle.style.animationduration=newDur + 's';                                                      // and setting the obstacle animation duration with newDur
                 console.log("new Animation duration ",newDur)
             },500)
         }
 },100)
 
-function updateScore(score)
-{
-    console.log(score);
-    scoreCount.innerHTML="Your Score :" + score;
+function updateScore(score)                                            // This is the function updateScore we are calling it from the non collision
+{                                                                      // condition line 74
+    console.log(score);                                               
+    scoreCount.innerHTML="Your Score :" + score;                       // here we targeting the ScoreCount div and setting the innerHTML as our 
+                                                                       // updated Score so that on the Screen we get the updated score 
 }
