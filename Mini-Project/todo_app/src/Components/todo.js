e60f3ee2c61ca92c908acd12518de18d8ff5b232
GarvG <67434601+GarvG@users.noreply.github.com>
2023-01-22 23:27:04 +0530

    Mini-Project

---
+++ Mini-Project/todo_app/src/Components/todo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function todo() {
+  return (
+    <>
+
+        <div className='Main-Container'>
+            <div className='Container'>
+                <figure>
+                    <img src="" alt="todoApp"/>
+                    
+                </figure>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default todo
