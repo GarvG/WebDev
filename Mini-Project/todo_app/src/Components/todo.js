720aa91b94c9ca60949ec0c78884a6f1341ecef9
GarvG <67434601+GarvG@users.noreply.github.com>
2023-01-25 18:08:12 +0530

    Todo-app finised UI and Completed adding of list one by one

--- Mini-Project/todo_app/src/Components/todo.js
+++ Mini-Project/todo_app/src/Components/todo.js
@@ -1,19 +1,44 @@
-import React from 'react'
+import React ,{useState}from 'react'
 
-function todo() {
+function Todo() {
+  const [word,setword]=useState("");
+
+  const [saveword,setsaveword]=useState([]);
+  const change=(e)=>{
+    console.log(e.target.value);
+    setword(e.target.value);
+  }
+
+  const Clicked=()=>{
+    setsaveword((olditem)=>{
+      return [...olditem,word]
+    })
+  }
   return (
     <>
 
-        <div className='Main-Container'>
-            <div className='Container'>
-                <figure>
-                    <img src="" alt="todoApp"/>
-                    
-                </figure>
-            </div>
-        </div>
+<div className="main-div">
+
+<div className="center-div">
+  <h1> TO DO LIST</h1>
+  <br/>
+  <input type="text" placeholder="Add a item" onChange={change}/>
+  <button onClick={Clicked}> + </button>
+  <ol>
+    {/* <li>
+      {word}
+    </li> */}
+    {saveword.map((currE)=>{
+      return (
+        <li>{currE}</li>
+      )
+    })}
+  </ol>
+  
+</div>
+</div>
     </>
   )
 }
 
-export default todo
+export default Todo
