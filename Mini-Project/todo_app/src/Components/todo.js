06c24e8a05177ad56d02d8ba406137b425a52b5a
GarvG <67434601+GarvG@users.noreply.github.com>
2023-01-26 18:48:32 +0530

    Completed removing the elements, Completed My todo App

--- Mini-Project/todo_app/src/Components/todo.js
+++ Mini-Project/todo_app/src/Components/todo.js
@@ -1,4 +1,5 @@
 import React ,{useState}from 'react'
+import Elements from './Elements';
 
 function Todo() {
   const [word,setword]=useState("");                               // this use State is used for getting the word 
@@ -14,8 +15,20 @@ function Todo() {
   const Clicked=()=>{                                             // using the second use State when we clicked the button for adding  more 
     setsaveword((olditem)=>{                                      // task we are using the setsaveword which has an empty array as of now    
       return [...olditem,word]                                    // so we return that array to saveword and in the array using the spread operator
-    })                                                            // we put all the old items and the new items which is store in word variable 
-                                                                
+    });                                                            // we put all the old items and the new items which is store in word variable 
+     setword(""); 
+  };
+
+
+
+  const deleteItems=(id)=>{
+    
+    setsaveword((olditem)=>{
+      return olditem.filter((arrEle,idx)=>{
+        return id!==idx;
+      })
+    })
+
   }
 
   return (
@@ -26,14 +39,23 @@ function Todo() {
 <div className="center-div">
   <h1> TO DO LIST</h1>
   <br/>
-  <input type="text" placeholder="Add a item" onChange={change}/>             {/* here in the input field we are using the change function  */}
+  <input type="text" placeholder="Add a item" onChange={change}
+    value={word}
+  />                                                                               {/* here in the input field we are using the change function  */}
                                                                                                     
-  <button onClick={Clicked}> + </button>                                          {/* here on the button we are using the clicked section */}
+  <button onClick={Clicked}> + </button>  
+                                              {/* here on the button we are using the clicked section */}
   <ol>
-    {saveword.map((currE)=>{                                            {/* using the map word we are iterating over the saveword array to display it in li  */}      
-      return (
-        <li>{currE}</li>
-      )
+    {
+      saveword.map((currE,i)=>{                                                 
+     return(
+      <Elements 
+      text={currE}
+      key={i}
+      id={i}
+      onSelect={deleteItems}
+      />
+     )
     })}
   </ol>
   
