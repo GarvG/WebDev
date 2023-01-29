99f67a8b15a06e735dd8c02223fc590b93bd81dc
GarvG <67434601+GarvG@users.noreply.github.com>
2023-01-30 00:00:14 +0530

    s

--- Reactjs/my-app/src/App.js
+++ Reactjs/my-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { Component } from 'react'
 import Forms from './Components/Forms';
+import {BrowserRouter} from 
 // import {StyledButton} from './Components/Button/Button.styles';
 // import { FancyButton } from './Components/Button/Button.styles';
 //import Example from './Components/Example';
@@ -13,7 +14,12 @@ import Forms from './Components/Forms';
 export class App extends Component {
   render() {
     return (
-      <div><Forms/></div>
+      <div>
+      <Browser>
+      <Forms/>
+      <h1> REACT ROUTER </h1>
+      <p> Without refresing the page we navigate</p> 
+      </div>
     )
   }
 }
