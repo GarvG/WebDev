c583d1a877b2558a2e76c0a8e136a3b43465c581
GarvG <67434601+GarvG@users.noreply.github.com>
2023-02-07 23:05:22 +0530

    Backend Async

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,18 @@
-const http=require('http');
-http.createServer((req,res)=>{
-res.write("HW! sup");
-res.end();
-}).listen(5000);
+// const http=require('http');
+// http.createServer((req,res)=>{
+// res.write("HW! sup");
+// res.end();
+// }).listen(5000);
+let a=10;
+let b=20;
 
+console.log('Start');
+
+setTimeout(()=>{
+    console.log('middle');
+},2000)
+
+setTimeout(()=>{
+    console.log('end');
+},2000)
+console.log(a+b);
